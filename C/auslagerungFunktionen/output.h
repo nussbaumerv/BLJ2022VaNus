97b8d9c8ebde8f2c361ab69bc4114b2b5cf26bc1
Your Name <you@example.com>
2022-09-02 09:27:10 +0200

    wow

---
+++ C/auslagerungFunktionen/output.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   output.h
+ * Author: nussb
+ *
+ * Created on 2. September 2022, 09:18
+ */
+
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OUTPUT_H */
+
