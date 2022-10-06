5bfd1d0d4300ead8ffa469148df50c0db04a643c
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-10-06 11:04:36 +0200

    final

--- C/multiArray/main.c
+++ C/multiArray/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ARRAYLENGTH 3
+
+#define ARRAYLENGTH 4
 
 
 int main() {
@@ -9,12 +10,11 @@ int main() {
     int x = 0;
     int y = 0;
     int saveValue = 0;
-    int arr[ARRAYLENGTH][ARRAYLENGTH] = {{0, 0, 0},
-                                         {0, 0, 0}};
+    int arr[ARRAYLENGTH][ARRAYLENGTH] = {{0}};
     do {
         printf("What feauture do you want to use?\n");
-        printf("1: Add Value\n");
-        printf("2: Remove Value\n");
+        printf("1 -> Add Value\n");
+        printf("2 -> Remove Value\n");
         scanf("%d", &whichFunc);
         fflush(stdin);
 
@@ -24,7 +24,7 @@ int main() {
         printf("Y Value:");
         scanf("%d", &y);
 
-        if(1 == whichFunc) {
+        if (1 == whichFunc) {
             printf("Save Value:");
             scanf("%d", &saveValue);
             fflush(stdin);
@@ -32,19 +32,29 @@ int main() {
             if (arr[x][y] == 0) {
                 arr[x][y] = saveValue;
                 printf("The Number %d got Added\n", saveValue);
+            } else {
+                printf("The Number already has a value reset it first.\n");
             }
-            else{
-                printf("Run the other Feautre for that.\n");
+        } else if (2 == whichFunc) {
+            if (0 == arr[x][y]) {
+                printf("The number is already reseted\n");
+            } else {
+                arr[x][y] = 0;
+                printf("The number got reseted\n");
             }
         }
-        else if(2 == whichFunc){
-            arr[x][y] = 0;
-            printf("You're number got reseted\n");
+        printf("\nYour array So far:\n");
+        printf("--------------------------------------------------------\n");
+        for(int c = 0; c < ARRAYLENGTH; c++) {
+            for (int i = 0; i < ARRAYLENGTH; i++) {
+                printf("| %d ", arr[c][i]);
+            }
+            printf("|\n");
+            printf("--------------------------------------------------------\n");
         }
-        printf("Do you want to do id again?\nType 1 if you want to:");
+        printf("\nDo you want to do id again?\nType 1 if you want to:");
         scanf("%d", &keepDoing);
-    }
-    while(keepDoing == 1);
+    } while (keepDoing == 1);
 
     system("pause");
 }
