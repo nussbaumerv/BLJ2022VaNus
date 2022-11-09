74476fcdc0053d8578a25abbd092738c9939d0f3
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-11-09 11:08:33 +0100

    updated input file

--- Java/KW45/Input/src/Main.java
+++ Java/KW45/Input/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Person tim = new Person("Tim", "Red");
-        tim.presentYou();
         tim.setFavoirteColor("red");
+        tim.presentYou();
+
+        Person tim2 = tim;
         tim.presentYou();
 
     }
