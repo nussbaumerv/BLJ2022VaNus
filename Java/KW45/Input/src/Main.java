1c4b971051d949c04cf3c05370759b89f88d2d26
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-11-09 10:57:38 +0100

    creted input file

---
+++ Java/KW45/Input/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Person tim = new Person("Tim", "Red");
+        tim.presentYou();
+        tim.setFavoirteColor("red");
+        tim.presentYou();
+
+    }
+}
