f55c78e74a5831f5f4c0064be8e9a8564808997d
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-11-11 15:52:16 +0100

    updated morse code

--- Java/KW45/MorseCode/src/Starter.java
+++ Java/KW45/MorseCode/src/Starter.java
@@ -6,22 +6,57 @@ public class Starter {
         System.out.println("Welcome to my translator");
         System.out.println("*************************************");
 
+        Scanner scan = new Scanner(System.in);
         boolean keepDoing = true;
 
         do {
-            System.out.println("\nType EXIT to exit Program");
-            System.out.print("Enter String: ");
-            Scanner scan = new Scanner(System.in);
-            String input = scan.nextLine();
-            String inputUpper = input.toUpperCase();
-            if(inputUpper.equals("EXIT") || inputUpper.equals("X")){
-                keepDoing = false;
+            System.out.println("" +
+                    "1.  From Text to Morse\n" +
+                    "2.  From Morse to Text\n" +
+                    "3.  EXIT\n");
+            System.out.print("Enter specific number: ");
+            int chooseFeautre = 0;
+            boolean valid = true;
+
+            try {
+                chooseFeautre = scan.nextInt();
+                scan.nextLine();
             }
-            else {
-                Translator newTranslate = new Translator();
-                String result = newTranslate.translateToMorse(inputUpper);
-                System.out.println(result);
+            catch(InputMismatchException ime){
+                valid = false;
             }
+
+            if(valid == true){
+                if(chooseFeautre == 1 || chooseFeautre == 2){
+                    if(chooseFeautre == 1){
+                        System.out.print("Enter Text: ");
+                    }else{
+                        System.out.print("Enter Morse: ");
+                    }
+                    String input = scan.nextLine();
+                    String inputUpper = input.toUpperCase();
+
+                    System.out.println("\n*************************************\n");
+
+                    Translator newTranslate = new Translator();
+                    String result = newTranslate.translateToMorse(chooseFeautre, inputUpper);
+                    System.out.println(result);
+                }
+                else if(chooseFeautre == 3){
+                    keepDoing = false;
+                }
+                else{
+                    System.out.println("\n*************************************\n");
+                    System.out.println("Please enter a valid input");
+                }
+            }
+            else{
+                System.out.println("\n*************************************\n");
+                System.out.println("Please enter a valid input");
+
+            }
+            System.out.println("\n*************************************\n");
+
         }while(keepDoing == true);
 
     }
--- Java/KW45/MorseCode/src/Translator.java
+++ Java/KW45/MorseCode/src/Translator.java
@@ -1,70 +1,88 @@
 import java.util.HashMap;
 import java.util.Map.Entry;
+
 public class Translator {
-public static String translateToMorse(String input){
-    HashMap<String, String> morseMap = new HashMap<String, String>();
+    public static String translateToMorse(int method, String input) {
+        HashMap<String, String> morseMap = new HashMap<String, String>();
 
-    boolean method = true;
+        morseMap.put("A", ".-");
+        morseMap.put("B", "-...");
+        morseMap.put("C", "-.-.");
+        morseMap.put("D", "-..");
+        morseMap.put("E", ".");
+        morseMap.put("F", "..-.");
+        morseMap.put("G", "--.");
+        morseMap.put("H", "....");
+        morseMap.put("I", "..");
+        morseMap.put("J", ".---");
+        morseMap.put("K", "-.-");
+        morseMap.put("L", ".-..");
+        morseMap.put("M", "--");
+        morseMap.put("N", "-.");
+        morseMap.put("O", "---");
+        morseMap.put("P", ".--.");
+        morseMap.put("Q", "--.-");
+        morseMap.put("R", ".-.");
+        morseMap.put("S", "...");
+        morseMap.put("T", "-");
+        morseMap.put("U", "..-");
+        morseMap.put("V", "...-");
+        morseMap.put("W", ".--");
+        morseMap.put("X", "-..-");
+        morseMap.put("Y", "-.--");
+        morseMap.put("Z", "--..");
 
+        morseMap.put("1", ".----");
+        morseMap.put("2", "..---");
+        morseMap.put("3", "...--");
+        morseMap.put("4", "....-");
+        morseMap.put("5", ".....");
+        morseMap.put("6", "-....");
+        morseMap.put("7", "--...");
+        morseMap.put("8", "---..");
+        morseMap.put("9", "----.");
+        morseMap.put("0", "-----");
 
-    morseMap.put("A", ".-");
-    morseMap.put("B", "-...");
-    morseMap.put("C", "-.-.");
-    morseMap.put("D", "-..");
-    morseMap.put("E", ".");
-    morseMap.put("F", "..-.");
-    morseMap.put("G", "--.");
-    morseMap.put("H", "....");
-    morseMap.put("I", "..");
-    morseMap.put("J", ".---");
-    morseMap.put("K", "-.-");
-    morseMap.put("L", ".-..");
-    morseMap.put("M", "--");
-    morseMap.put("N", "-.");
-    morseMap.put("O", "---");
-    morseMap.put("P", ".--.");
-    morseMap.put("Q", "--.-");
-    morseMap.put("R", ".-.");
-    morseMap.put("S", "...");
-    morseMap.put("T", "-");
-    morseMap.put("U", "..-");
-    morseMap.put("V", "...-");
-    morseMap.put("W", ".--");
-    morseMap.put("X", "-..-");
-    morseMap.put("Y", "-.--");
-    morseMap.put("Z", "--..");
+        morseMap.put(" ", "/");
 
-    morseMap.put("1", ".----");
-    morseMap.put("2", "..---");
-    morseMap.put("3", "...--");
-    morseMap.put("4", "....-");
-    morseMap.put("5", ".....");
-    morseMap.put("6", "-....");
-    morseMap.put("7", "--...");
-    morseMap.put("8", "---..");
-    morseMap.put("9", "----.");
-    morseMap.put("0", "-----");
-
-    morseMap.put(" ", "/");
-
-    if(method) {
-        char inputArr[] = input.toCharArray();
-
-        String temp;
+        String before;
         String result = "";
-        for (int i = 0; i < inputArr.length; i++) {
-            temp = Character.toString(inputArr[i]);
-            result = result + morseMap.get(temp) + " ";
-        }
-        return result;
-    }
-    else{
-        for(Entry<String, String> entry: morseMap.entrySet()) {
-            if(entry.getValue() == ) {
-                System.out.println("The key for value " + value + " is " + entry.getKey());
-                break;
+        String translated = "";
+        boolean successful = true;
+
+        if (method == 1) {
+            char inputArr[] = input.toCharArray();
+            for (int i = 0; i < inputArr.length; i++) {
+                before = Character.toString(inputArr[i]);
+                translated = morseMap.get(before);
+                if(translated == null || translated.isEmpty()){
+                    successful = false;
+                }
+                result = result + translated + " ";
+            }
+        } else {
+            String[] inputArr = null;
+            inputArr = input.split(" ");
+
+            for (int i = 0; i < inputArr.length; i++) {
+                before = inputArr[i];
+                for (Entry<String, String> entry : morseMap.entrySet()) {
+                    if (entry.getValue().equals(before)) {
+                        translated = entry.getKey();
+                        if(translated == null || translated.isEmpty()){
+                            successful = false;
+                        }
+                        result = result + translated;
+                        break;
+                    }
+                }
             }
         }
+        if(successful){
+            return result;
+        }
+        else {
+            return "Please enter Valid input";
+        }
     }
-}
 }
