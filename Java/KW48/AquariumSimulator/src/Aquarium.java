435fef2ee3e72d1688a38cf44a5e38dc0e1e6494
Valentin Nussbaumer <valentin.nussbaumer@edu.tbz.ch>
2022-12-01 11:17:18 +0100

    added fillment feauture

--- Java/KW48/AquariumSimulator/src/Aquarium.java
+++ Java/KW48/AquariumSimulator/src/Aquarium.java
@@ -3,29 +3,55 @@ import java.util.*;
 public class Aquarium {
     private int columns;
     private int rows;
+
+    private int rowsAvailable;
     private boolean salty;
     private int[][] aquarium;
-    public Aquarium(boolean salty, int cols, int rows){
+    public Aquarium(boolean salty, int cols, int rows, int rowsAvailable){
         this.columns = cols;
         this.rows = rows;
         this.salty = salty;
-        this.aquarium= new int[columns][rows];
+        this.aquarium= new int[rows][columns];
+        this.rowsAvailable = rowsAvailable;
+    }
+    public int checkIfFree(int x, int y){
+        int state = 0;
+        if(aquarium[x][y] == 1){
+            state = 1;
+        }
+        else if(aquarium[x][y] == 2){
+            state = 2;
+        }
+        else if(aquarium[x][y] == 0){
+            state = 0;
+        }
+        else{
+            state = 8;
+        }
+        return state;
     }
     public void updateAquarium(ArrayList<Fish> fishs) {
-        for (int col = 0; col < aquarium.length; ++col) {
-            for (int row = 0; row < aquarium[col].length; ++row) {
-                aquarium[col][row] = 0;
+        int waterFree = rows - rowsAvailable;
+        for (int row = 0; row < aquarium.length; ++row) {
+            for (int col = 0; col < aquarium[row].length; ++col) {
+                if(waterFree > 0){
+                    aquarium[row][col] = 2;
+                }
+                else{
+                    aquarium[row][col] = 0;
+                }
             }
+            waterFree--;
         }
 
         for (int i = 0; i < fishs.size(); i++) {
             int x = fishs.get(i).getPositionX();
             int y = fishs.get(i).getPositionY();
 
-            for (int col = 0; col < aquarium.length; ++col) {
-                for (int row = 0; row < aquarium[col].length; ++row) {
+            for (int row = 0; row < aquarium.length; ++row) {
+                for (int col = 0; col < aquarium[row].length; ++col) {
                     if (col == y && row == x) {
-                        aquarium[col][row] = 1;
+                        aquarium[row][col] = 1;
                     }
                 }
             }
@@ -33,16 +59,16 @@ public class Aquarium {
     }
 
     public void printAquarium() {
-        for (int i = 0; i < (2+rows); i++) {
+        for (int i = 0; i < (2+columns); i++) {
             System.out.print("⬜ ");
         }
         System.out.println("");
         String aquariumAsString = Arrays.deepToString(aquarium);
         aquariumAsString = aquariumAsString.replace("[[", "⬜ ").replace("], [", " ⬜\n⬜ ").replace(", ", " ").replace("]]",
-                " ⬜").replace("0", "\uD83D\uDFE6").replace("1", "\uD83D\uDC1F");
+                " ⬜").replace("0", "\uD83D\uDFE6").replace("1", "\uD83D\uDC1F").replace("2", "⬛");
         System.out.println(aquariumAsString);
 
-        for (int i = 0; i < (2+rows); i++) {
+        for (int i = 0; i < (2+columns); i++) {
             System.out.print("⬜ ");
         }
         System.out.println("");
--- Java/KW48/AquariumSimulator/src/Starter.java
+++ Java/KW48/AquariumSimulator/src/Starter.java
@@ -18,6 +18,8 @@ public class Starter {
         y = scan.nextInt();
         scan.nextLine();
 
+
+
         System.out.println("\nIn what water Type should your Fish live?");
         System.out.println("\n" +
                 "1.  Saltwater\n" +
@@ -46,13 +48,21 @@ public class Starter {
             System.out.println("Invalid Input\n Saltwater was chosen.");
             salty = true;
         }
-
-        if (aquarium.isSalty() == salty) {
-            Fish fish = new Fish(fishCount, salty, race, x, y);
-            fishs.add(fish);
-        } else {
-            System.out.println("\nOh no. \uD83D\uDC80 \nYour fish died because you chose the wrong water type");
+        if(aquarium.checkIfFree(x,y) == 0) {
+            if (aquarium.isSalty() == salty) {
+                Fish fish = new Fish(fishCount, salty, race, x, y);
+                fishs.add(fish);
+            } else {
+                System.out.println("\nOh no. \uD83D\uDC80 \nYour fish died because you chose the wrong water type");
+            }
         }
+        else if(aquarium.checkIfFree(x,y) == 1){
+            System.out.println("\nOh no. \uD83D\uDC1F \nAt that position is already a fish!");
+        }
+        else{
+            System.out.println("\nOh no. \uD83D\uDC80 \nYour fish can only live in water!");
+        }
+
     }
 
     public static void moveFish(ArrayList<Fish> fishs) {
@@ -128,10 +138,7 @@ public class Starter {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println("\n-------------------------------------");
-        System.out.println("|     Welcome to my Aquarium        |");
-        System.out.println("-------------------------------------");
+    public static Aquarium createAquarium(){
         Scanner scan = new Scanner(System.in);
         int cols = 20;
         int rows = 10;
@@ -158,6 +165,44 @@ public class Starter {
             cols = 20;
             rows = 10;
         }
+        int volume = cols * rows;
+        System.out.println("You Aquarium ha a Capacity of " + volume + "l. \nHow many Percent do you want to fill it?");
+        System.out.println("\n" +
+                "1.  20%\n" +
+                "2.  40%\n" +
+                "3.  60%\n" +
+                "4.  80%\n" +
+                "5.  100%\n");
+        System.out.print("Enter specific number: ");
+
+        int fillment = 0;
+        double yAvailable;
+        valid = true;
+        try {
+            fillment = scan.nextInt();
+        } catch (InputMismatchException ime) {
+            valid = false;
+        }
+        scan.nextLine();
+
+        if (fillment == 1) {
+            yAvailable = rows * 0.2;
+        } else if (fillment == 2) {
+            yAvailable = rows * 0.4;
+        }  else if (fillment == 3) {
+            yAvailable = rows * 0.6;
+        } else if (fillment == 4) {
+            yAvailable = rows * 0.8;
+        } else if (fillment == 5) {
+            yAvailable = rows;
+        }else if (valid == false) {
+            System.out.println("Invalid Input\n100% was chosen.");
+            yAvailable = rows;
+        } else {
+            System.out.println("Invalid Input\n100% was chosen.");
+            yAvailable = rows;
+        }
+        int rowsAvailable = (int)yAvailable;
 
         System.out.println("\nWhat water Type do you want?");
         System.out.println("\n" +
@@ -188,10 +233,21 @@ public class Starter {
             salty = true;
         }
 
-        Aquarium aquarium = new Aquarium(salty, cols, rows);
+        Aquarium aquarium = new Aquarium(salty, cols, rows, rowsAvailable);
+        return aquarium;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\n-------------------------------------");
+        System.out.println("|     Welcome to my Aquarium        |");
+        System.out.println("-------------------------------------");
+        Scanner scan = new Scanner(System.in);
+
+        Aquarium aquarium = createAquarium();
         ArrayList<Fish> fishs = new ArrayList<Fish>();
         int fishCount = 0;
         boolean keepDoing = true;
+        boolean valid;
 
         do {
             System.out.println("\n" +
@@ -200,7 +256,8 @@ public class Starter {
                     "3.  Move Fish\n" +
                     "4.  Remove Fish\n" +
                     "5.  Print Information about your fish\n" +
-                    "6.  Exit\n");
+                    "6.  Change capacity\n" +
+                    "7.  Exit\n");
             System.out.print("Enter specific number: ");
             int option = 0;
 
@@ -227,8 +284,10 @@ public class Starter {
             } else if (option == 5) {
                 infoFish(fishs);
             } else if (option == 6) {
+
+            }else if (option == 7) {
                 keepDoing = false;
-            } else {
+            }  else {
                 System.out.println("Please Enter a valid input");
             }
         }
