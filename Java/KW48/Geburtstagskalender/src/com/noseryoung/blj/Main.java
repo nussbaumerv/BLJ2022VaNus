276f3392edcd15f048615dea2e7ce7fb0b48df0a
Valentin Nussbaumer <valentin.nussbaumer@edu.tbz.ch>
2022-12-02 09:48:58 +0100

    created Geburtstagklaender

---
+++ Java/KW48/Geburtstagskalender/src/com/noseryoung/blj/Main.java
@@ -0,0 +1,7 @@
+package com.noseryoung.blj;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java/KW48/Sitzplatzreservierung-12/src/com/noseryoung/blj/Main.java
@@ -0,0 +1,10 @@
+package com.noseryoung.blj;
+
+public class Main {
+    public static int createArena(int rows){
+        return 1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
