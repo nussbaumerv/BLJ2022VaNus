65874bd693c337d77b949e0e91ad2a385de0938f
Valentin Nussbaumer <valentin.nussbaumer@edu.tbz.ch>
2022-12-21 15:53:41 +0100

    added ranking with https

--- Java/KW51/Wordle/src/com/noseryoung/blj/Main.java
+++ Java/KW51/Wordle/src/com/noseryoung/blj/Main.java
@@ -2,9 +2,11 @@ package com.noseryoung.blj;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.net.*;
+import java.io.*;
 
 public class Main {
-    public static void wordleManger(Words words, Field field, String option) {
+    public static void wordleManger(Words words, Field field, String option, String name) throws Exception {
         Scanner scan = new Scanner(System.in);
         String correctWord = words.getWord();
         correctWord = correctWord.toUpperCase();
@@ -32,6 +34,9 @@ public class Main {
                     if (answer) {
                         System.out.println("\nNice job!");
                         System.out.println("You were able to solve it in " + counter + " tries\n");
+                        try (java.util.Scanner s = new java.util.Scanner(new java.net.URL("https://valentin-nussbaumer.com/wordle/insert.php?name="+ name + "&score=" + counter + "").openStream())) {
+                            System.out.println(s.useDelimiter("\\A").next());
+                        }
                         exit = true;
                     } else {
                         System.out.println("\nNot quite\n");
@@ -51,31 +56,39 @@ public class Main {
 
         }
     }
-        public static void main (String[]args) throws IOException {
+        public static void main (String[]args) throws Exception {
             boolean keepDoing = true;
             Field field = new Field();
             Words words = new Words();
             Scanner scan = new Scanner(System.in);
 
-            String welcomeText = "Welcome to WORDLE";
-            String[] welcomeArr = welcomeText.split("", 17);
             System.out.println("");
             System.out.println("\033[0;107m\033[1;90m W E L C O M E \033[0m  \033[0;103m\033[1;90m T O \033[0m  \033[0;102m\033[1;90m W O R D L E \033[0m\n");
 
+            System.out.print("Enter your Name: ");
+            String name = scan.nextLine();
+
 
             do {
-                System.out.println("What do you want to do?");
+                System.out.println("\nWhat do you want to do?");
                 System.out.println("" +
                         "1. Solve a classic WORDLE\n" +
                         "2. Solve a WORDLE without Word check\n" +
-                        "3. Exit");
+                        "3. Print Leaderboard\n" +
+                        "4. Exit");
                 System.out.print("Enter Option: ");
                 String option = scan.nextLine();
                 if (option.equals("1")) {
-                    wordleManger(words, field, option);
+                    wordleManger(words, field, option, name);
                 } else if (option.equals("2")) {
-                    wordleManger(words, field, option);
-                } else if (option.equals("3")) {
+                    wordleManger(words, field, option, name);
+                } else if (option.equals("3")){
+                    System.out.println("\nLeaderboard");
+                    try (java.util.Scanner s = new java.util.Scanner(new java.net.URL("https://valentin-nussbaumer.com/wordle/").openStream())) {
+                        String result = s.useDelimiter("\\A").next();
+                        System.out.println(result);
+                    }
+                } else if (option.equals("4")) {
                     System.out.println("Thanks for using my Program");
                     keepDoing = false;
                 } else {
