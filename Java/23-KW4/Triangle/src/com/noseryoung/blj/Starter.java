3462cf1c164616f925475c41ccee82dc7c7ce49b
Valentin Nussbaumer <valentin.nussbaumer@edu.tbz.ch>
2023-01-25 15:32:20 +0100

    many changes to Triangle

---
+++ Java/23-KW4/Triangle/src/com/noseryoung/blj/Starter.java
@@ -0,0 +1,10 @@
+package com.noseryoung.blj;
+
+import com.noseryoung.blj.exceptions.TriangleException;
+
+public class Starter {
+  public static void main(String[] args) throws TriangleException {
+    TriangleApp trilangle = new TriangleApp("Google", "Ka");
+    trilangle.start();
+  }
+}
