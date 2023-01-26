019c9c32e060bb3975f6611b23b70e63ec832f50
Valentin Nussbaumer <valentin.nussbaumer@edu.tbz.ch>
2023-01-26 09:37:44 +0100

    many updates to theme

--- Java/23-KW4/Triangle/src/com/noseryoung/blj/TriangleApp.java
+++ Java/23-KW4/Triangle/src/com/noseryoung/blj/TriangleApp.java
@@ -7,146 +7,212 @@ import java.util.Scanner;
 
 public class TriangleApp {
 
-  public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-  public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-  public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-  public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    private String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    private String ANSI_RED_BACKGROUND = "\u001B[41m";
+    private String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    private String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    private String ANSI_GREEN_BACKGROUND = "\u001B[42m";
 
-  private String company;
-  private String handler;
-  private double version;
+    private String company;
+    private String handler;
+    private double version;
 
-  private boolean isRunning;
+    private String codeTable = "\n" +
+            "***********************************************\n" +
+            "| Codes   | Type \n" +
+            "***********************************************\n" +
+            "| " + ANSI_GREEN_BACKGROUND + "TRI84TF" + ANSI_BLACK_BACKGROUND + " | Gleichschenkliges Dreieck\n" +
+            "-----------------------------------------------\n" +
+            "| " + ANSI_GREEN_BACKGROUND + "TRI66TF" + ANSI_BLACK_BACKGROUND + " | Gleichseitiges Dreieck\n" +
+            "-----------------------------------------------\n" +
+            "| " + ANSI_GREEN_BACKGROUND + "TRI60TF" + ANSI_BLACK_BACKGROUND + " | Gewöhnliches Dreieck\n" +
+            "-----------------------------------------------\n" +
+            "| " + ANSI_GREEN_BACKGROUND + "TRI72TF" + ANSI_BLACK_BACKGROUND + " | Rechtwinkliges Dreieck\n" +
+            "-----------------------------------------------\n" +
+            "| " + ANSI_RED_BACKGROUND + "ERR36TF" + ANSI_BLACK_BACKGROUND +   " | Unmögliches Dreieck\n" +
+            "-----------------------------------------------\n" +
+            "| " + ANSI_RED_BACKGROUND + "ERR96TF" + ANSI_BLACK_BACKGROUND +   " | Buchstaben oder sonstige Zeichen\n" +
+            "-----------------------------------------------\n" +
+            "| " + ANSI_RED_BACKGROUND + "ERR12TF" + ANSI_BLACK_BACKGROUND +   " | Minuszahlen\n" +
+            "-----------------------------------------------\n" +
+            "| " + ANSI_RED_BACKGROUND + "ERR16TF" + ANSI_BLACK_BACKGROUND +   " | Null als Seitenlänge\n" +
+            "-----------------------------------------------\n" +
+            "| " + ANSI_RED_BACKGROUND + "ERR56TF" + ANSI_BLACK_BACKGROUND +   " | Eine Gerade\n" +
+            "-----------------------------------------------\n";
 
-  private String sideAInput;
-  private String sideBInput;
-  private String sideCInput;
+    private String triangleGraphic = "";
+    private String codeBgColor = ANSI_RED_BACKGROUND;
 
-  private double sideA;
-  private double sideB;
-  private double sideC;
+    private boolean isRunning;
 
-  private String code;
+    private String sideAInput;
+    private String sideBInput;
+    private String sideCInput;
 
-  public TriangleApp(String company, String handler, double version) {
-    this.company = company;
-    this.handler = handler;
-    this.version = version;
-    this.isRunning = true;
-  }
+    private double sideA;
+    private double sideB;
+    private double sideC;
 
-  public void start() throws TriangleException {
-    printHeader();
-    while (isRunning) {
-      System.out.println("\nTEST CASES TRIANGLE\n");
+    private String code;
 
-      sideAInput = promptSide("a");
-      sideBInput = promptSide("b");
-      sideCInput = promptSide("c");
-
-
-      try {
-        validateInput();
-        code = determineTriangleType();
-      }
-      catch (TriangleException e) {
-        code = e.getMessage();
-      }
-      finally {
-        printResult();
-      }
-      promptAction();
-    }
-  }
-
-  private void printHeader() {
-    System.out.println(ANSI_BLACK_BACKGROUND + "******************************");
-    System.out.println(ANSI_BLUE_BACKGROUND +  "      TRIANGLE EVALUATOR      " + ANSI_BLACK_BACKGROUND);
-    System.out.println("******************************");
-    System.out.println( "COMPANY: " + ANSI_CYAN_BACKGROUND + company + ", " + Calendar.getInstance().get(Calendar.YEAR) + ANSI_BLACK_BACKGROUND );
-    System.out.println("HANDLER: " + ANSI_CYAN_BACKGROUND  + handler + ANSI_BLACK_BACKGROUND );
-    System.out.println("VERSION: " + ANSI_CYAN_BACKGROUND + version + ANSI_BLACK_BACKGROUND );
-    System.out.println( ANSI_BLACK_BACKGROUND + "******************************");
-  }
-
-  private void printResult() {
-    System.out.println("\n" + ANSI_RED_BACKGROUND + " Code: " + code + " " + ANSI_BLACK_BACKGROUND);
-  }
-  private String promptSide(String side) {
-    System.out.print("Enter Side " + side + ": ");
-    Scanner scan = new Scanner(System.in);
-    String input = scan.next();
-    return input;
-  }
-
-  private void promptAction() {
-    Scanner scan = new Scanner(System.in);
-    String action = "";
-    boolean valid = false;
-
-    do{
-      System.out.println("" +
-              "\nWhat do you want to do?\n" +
-                      "<c> Continue \n" +
-                      "<q> Quit \n");
-     action = scan.nextLine();
-     action.toLowerCase();
-     if(action.equals("q") || action.equals("c")){
-       valid = true;
-     }
-    } while (valid == false);
-
-    if(action.equals("q")){
-      isRunning = false;
+    public TriangleApp(String company, String handler, double version) {
+        this.company = company;
+        this.handler = handler;
+        this.version = version;
+        this.isRunning = true;
     }
 
-	
-    System.out.println("******************************");
-  }
-
-  private void validateInput() throws TriangleException {
-    try {
-      sideA = Double.parseDouble(sideAInput);
-      sideB = Double.parseDouble(sideBInput);
-      sideC = Double.parseDouble(sideCInput);
-    }
-    catch(NumberFormatException e){
-      throw new IllegalTriangleSideException();
-    }
-    // validate sideAInput, sideBInput, sideCInput as double
-
-    if (sideA == 0 || sideB == 0 || sideC == 0) {
-      throw new ZeroTriangleSideException();
-    }
-    if (sideA < 0 || sideB < 0 || sideC < 0) {
-      throw new NegativeTriangleSideException();
-    }
-
-    if(sideA + sideB < sideC || sideA + sideC < sideB || sideB + sideC < sideA){
-      throw new ImpossibleTriangleException();
-    }
-    if(sideA + sideB == sideC || sideA + sideC == sideB || sideB + sideC == sideA){
-      throw new TriangleIsLineException();
-    }
-  }
-
-  private String determineTriangleType() {
-    String returnCode;
-
-    double sideAPOW = Math.pow(sideA, 2);
-    double sideBPOW = Math.pow(sideB, 2);
-    double sideCPOW = Math.pow(sideC, 2);
+    public void start() throws TriangleException {
+        printHeader();
+        while (isRunning) {
+            System.out.println("\n------------------------------");
+            System.out.println(ANSI_BLUE_BACKGROUND + "      TEST CASES TRIANGLE     " + ANSI_BLACK_BACKGROUND);
+            System.out.println("------------------------------\n");
 
 
-    if (sideA == sideB && sideB == sideC) {
-    returnCode = "TRI66TF";
-    } else if(sideA == sideB || sideB == sideC || sideA == sideC){
-      returnCode = "TRI84TF";
-    }else if(sideAPOW + sideBPOW == sideCPOW || sideCPOW + sideBPOW == sideAPOW || sideAPOW + sideCPOW == sideBPOW){
-      returnCode = "TRI72TF";
-    } else{
-      returnCode = "TRI60TF";
+            sideAInput = promptSide("a");
+            sideBInput = promptSide("b");
+            sideCInput = promptSide("c");
+
+
+            try {
+                validateInput();
+                code = determineTriangleType();
+            } catch (TriangleException e) {
+                code = e.getMessage();
+            } finally {
+                printResult();
+            }
+            promptAction();
+        }
     }
-    return returnCode;
-  }
+
+    private void printHeader() {
+        System.out.println(ANSI_BLACK_BACKGROUND + "******************************");
+        System.out.println(ANSI_BLUE_BACKGROUND +  "      TRIANGLE EVALUATOR      " + ANSI_BLACK_BACKGROUND);
+        System.out.println("******************************");
+        System.out.println("COMPANY: " + ANSI_CYAN_BACKGROUND + company + ", " + Calendar.getInstance().get(Calendar.YEAR) + ANSI_BLACK_BACKGROUND);
+        System.out.println("HANDLER: " + ANSI_CYAN_BACKGROUND + handler + ANSI_BLACK_BACKGROUND);
+        System.out.println("VERSION: " + ANSI_CYAN_BACKGROUND + version + ANSI_BLACK_BACKGROUND);
+        System.out.println(ANSI_BLACK_BACKGROUND + "******************************");
+    }
+
+    private void printResult() {
+        System.out.println("\n" + codeBgColor + " Code: " + code + " " + ANSI_BLACK_BACKGROUND);
+        System.out.println(triangleGraphic);
+    }
+
+    private String promptSide(String side) {
+        System.out.print("Enter Side " + side + ": ");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        return input;
+    }
+
+
+    private void promptAction() {
+        Scanner scan = new Scanner(System.in);
+        String action = "";
+        boolean valid = false;
+
+        do {
+            codeBgColor = ANSI_RED_BACKGROUND;
+            triangleGraphic = "";
+
+            System.out.println("" +
+                    "\nWhat do you want to do?\n" +
+                    "<c> Continue \n" +
+                    "<o> Print codes\n" +
+                    "<q> Quit \n");
+            System.out.print("Option: ");
+            action = scan.nextLine();
+            action.toLowerCase();
+            if (action.equals("q") || action.equals("c")) {
+                valid = true;
+            } else if(action.equals("o")){
+                System.out.println(codeTable);
+            }
+        } while (valid == false);
+
+        if (action.equals("q")) {
+            isRunning = false;
+            System.out.println("\nThanks for using my program!");
+        }
+
+
+        System.out.println("******************************");
+    }
+
+    private void validateInput() throws TriangleException {
+        try {
+            sideA = Double.parseDouble(sideAInput);
+            sideB = Double.parseDouble(sideBInput);
+            sideC = Double.parseDouble(sideCInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalTriangleSideException();
+        }
+
+        if (sideA == 0 || sideB == 0 || sideC == 0) {
+            throw new ZeroTriangleSideException();
+        }
+        if (sideA < 0 || sideB < 0 || sideC < 0) {
+            throw new NegativeTriangleSideException();
+        }
+
+        if (sideA + sideB < sideC || sideA + sideC < sideB || sideB + sideC < sideA) {
+            throw new ImpossibleTriangleException();
+        }
+        if (sideA + sideB == sideC || sideA + sideC == sideB || sideB + sideC == sideA) {
+            throw new TriangleIsLineException();
+        }
+    }
+
+    private String determineTriangleType() {
+        String returnCode;
+        codeBgColor = ANSI_GREEN_BACKGROUND;
+
+        double sideAPOW = Math.pow(sideA, 2);
+        double sideBPOW = Math.pow(sideB, 2);
+        double sideCPOW = Math.pow(sideC, 2);
+
+
+        if (sideA == sideB && sideB == sideC) {
+            triangleGraphic = "\n" +
+                       "Gleichseitiges Dreieck \n\n" +
+                       "             / \\ \n" +
+                       "           /     \\ \n" +
+                       "         /         \\ \n" +
+                       "       /             \\ \n" +
+                       "      -----------------";
+            returnCode = "TRI66TF";
+        } else if (sideA == sideB || sideB == sideC || sideA == sideC) {
+            triangleGraphic = "\n" +
+                    "Gleichschenkliges Dreieck \n\n" +
+                    "          / \\ \n" +
+                    "         /   \\ \n" +
+                    "        /     \\ \n" +
+                    "       /       \\ \n" +
+                    "      -----------";
+            returnCode = "TRI84TF";
+        } else if (sideAPOW + sideBPOW == sideCPOW || sideCPOW + sideBPOW == sideAPOW || sideAPOW + sideCPOW == sideBPOW) {
+            triangleGraphic = "\n" +
+                    "Rechtwinkliges Dreieck \n\n" +
+                    "       | \\ \n" +
+                    "       |   \\ \n" +
+                    "       |     \\ \n" +
+                    "       |       \\ \n" +
+                    "       ----------";
+            returnCode = "TRI72TF";
+        } else {
+            triangleGraphic = "\n" +
+                    "Gewöhnliches Dreieck \n\n" +
+                    "    \\ \\ \n" +
+                    "     \\   \\ \n" +
+                    "      \\      \\ \n" +
+                    "       \\        \\ \n" +
+                    "       ------------";
+            returnCode = "TRI60TF";
+        }
+        return returnCode;
+    }
 }
